1a465d7b39a3aa83f7f25857e954928332dffe9d
gopae02 <gopae02@gmail.com>
2021-03-04 14:06:10 +0900

    # 3-10 게시물 수정 & 삭제 기능 추가하기
    https://wikidocs.net/81061

--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Blueprint, render_template, request, url_for
+from flask import Blueprint, render_template, request, url_for, g, flash
 from werkzeug.utils import redirect
 
 from .. import db
@@ -46,3 +46,42 @@ def create():
         db.session.commit()
         return redirect(url_for('main.index'))
     return render_template('question/question_form.html', form=form)
+
+# --------------------------------- [edit] ---------------------------------- #
+# modify 함수가 GET 방식으로 요청되는 경우는 <질문수정> 버튼을 눌렀을 때이다(question/question_form.html 템플릿 렌더링
+# 수정할 질문에 해당하는 ‘제목’, ‘내용’ 등의 데이터가 보여야 한다
+# 데이터베이스에서 조회한 데이터를 템 플릿에 적용하는 가장 간단한 방법은 QuestionForm(obj=question)과 같이
+# 조회한 데이터를 obj 매개변수에 전달하여 폼을 생성하는 것
+# QuestionForm의 subject, content 필드에 question 객체의 subject, content의 값이 적용
+@bp.route('/modify/<int:question_id>', methods=('GET', 'POST'))
+@login_required
+def modify(question_id):
+    question = Question.query.get_or_404(question_id)
+    if g.user != question.user:
+        # flash 함수는 강제로 오류를 발생시키는 함수로, 로직에 오류가 있을 경우 사용
+        flash('수정권한이 없습니다')
+        return redirect(url_for('question.detail', question_id=question_id))
+    if request.method == 'POST':
+        form = QuestionForm()
+        if form.validate_on_submit():
+            form.populate_obj(question)
+            question.modify_date = datetime.now()  # 수정일시 저장
+            db.session.commit()
+            return redirect(url_for('question.detail', question_id=question_id))
+    else:
+        form = QuestionForm(obj=question)
+    return render_template('question/question_form.html', form=form)
+# --------------------------------------------------------------------------- #
+
+# delete 함수 역시 로그인이 필요하므로 @login_required 애너테이션을 적용하고,
+# 로그인한 사용자와 글쓴이가 같은 경우에만 질문을 삭제할 수 있도록
+@bp.route('/delete/<int:question_id>')
+@login_required
+def delete(question_id):
+    question = Question.query.get_or_404(question_id)
+    if g.user != question.user:
+        flash('삭제권한이 없습니다')
+        return redirect(url_for('question.detail', question_id=question_id))
+    db.session.delete(question)
+    db.session.commit()
+    return redirect(url_for('question._list'))
